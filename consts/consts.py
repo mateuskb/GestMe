22f667ac235b443a3f3a96a1e15f51722ab9bc04
mateuskb <mateuskb@icloud.com>
2019-08-02 11:55:57 -0300

    First commit

---
+++ consts/consts.py
@@ -0,0 +1,10 @@
+class Consts:
+
+    RECOMMENDIT_DB = {
+        'hostname': "localhost",
+        'username': "mateuskb",
+        'password': "djrtdg33",
+        'database': "recommendit"
+    }
+
+    SGBDS = ['pgsql', 'mysql']
---
+++ inc/classes/lib/db.py
@@ -0,0 +1,16 @@
+import sys
+import psycopg2
+from psycopg2 import extras
+
+sys.path.insert(1, './../../../consts')
+
+from consts import Consts as consts
+
+class DbLib:
+
+    def __init__(self, sgbd='pgsql'):
+        self.sgbd = sgbd
+    
+    # def connect(db):
+
+print(consts.SGBDS)

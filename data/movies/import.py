96974281134a041836ec2fa9e39b0d4161dcb9db
mateuskb <mateuskb@icloud.com>
2019-08-22 02:40:45 -0300

    Data handling v3 - genres

--- data/movies/import.py
+++ data/movies/import.py
@@ -9,13 +9,14 @@ ratings = {}
 collections = {} 
 mov_col = {} # Add 1 to index
 mov_id = {} # Add 1 to index
+mov_gen = {} # Add 1 to index
 genres = {}
 movies = {} # Add 1 to index
 
 # Imports
 data_kw = pd.read_csv('keywords.csv', index_col='id')
 data_rt = pd.read_csv('ratings_small.csv', index_col='movieId')
-data_mv = pd.read_csv('movies_metadata.csv', low_memory=False)[:50]
+data_mv = pd.read_csv('movies_metadata.csv', low_memory=False)
 
 # --- Data Handling ---
 
@@ -51,15 +52,7 @@ mov_id = data_mv['id']
 data_mv = data_mv.set_index('id')
 
 # Collections
-# a = []
-# collections = list(dict.fromkeys(data_mv['belongs_to_collection']))
-
-# for col in collections:
-#     for c in col:
-#         # c = ast.literal_eval(c)
-#         a.append(c)
-
-# collections = a
+collections = list(dict.fromkeys(data_mv['belongs_to_collection']))
 
 col = data_mv['belongs_to_collection']
 for key, value in col.items():
@@ -67,22 +60,35 @@ for key, value in col.items():
 
     if isinstance(value, str): # Belong to a collection
         value = ast.literal_eval(value)
-        mov_col[key]['name'] = value['name']
-        mov_col[key]['poster_path'] = value['poster_path']
+        if isinstance(value, dict):
+            mov_col[key]['name'] = value['name']
+            mov_col[key]['poster_path'] = value['poster_path']
+        else:
+            mov_col[key] = None
     else:
         mov_col[key] = None
     
 # Genres
-# gen = data_mv['genres']
-# for key, value in col.items():
-#     collections[key] = {}
+a = []
+genres = list(dict.fromkeys(data_mv['genres']))
 
-#     if isinstance(value, str): # Belong to a collection
-#         value = ast.literal_eval(value)
-#         collections[key]['name'] = value['name']
-#         collections[key]['poster_path'] = value['poster_path']
-#     else:
-#         collections[key] = None
+for genres_list in genres:
+    genres_list = ast.literal_eval(genres_list)
+    for word in genres_list:
+        a.append(word['name'])
 
-print(a)
+genres = list(dict.fromkeys(a))
+
+gen = data_mv['genres']
+for key, values in gen.items():
+    values = ast.literal_eval(values)
+    mov_gen[key] = []
+    for item in values:
+        mov_gen[key].append(item['name'])
+
+# Movies
+
+
+
+# print(mov_col)
 

196dfdcf0283ed973d8c5a3e55d2a94ce5a1dc55
mateuskb <mateuskb@icloud.com>
2019-09-13 13:55:19 -0300

    Update

--- kivy/pages/app/home/home.py
+++ kivy/pages/app/home/home.py
@@ -1,9 +1,9 @@
 import sys, os
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
-from kivy.storage.jsonstore import JsonStore
 from kivy.uix.screenmanager import Screen
+from kivy.properties import StringProperty
+
 
 BASE_PATH = os.path.abspath(__file__+ '/../../../../')
 
@@ -15,7 +15,14 @@ from inc.consts.consts import Consts
 # Load KV file
 Builder.load_file(BASE_PATH + '/pages/app/home/home.kv')
 
+# class FullImage(Image):
+#         pass
+
+
 class HomeWindow(Screen):
+
+    base_path = 'http://image.tmdb.org/t/p/w185/'
+    image_path = base_path + '16XOMpEaLWkrcPqSQqhTmeJuqQl.jpg'
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -23,11 +30,14 @@ class HomeWindow(Screen):
         resp = Storage.r_authtoken()
         if not resp:
             self.logout()   
-        
 
     def consts(self):
         return Consts()
     
+    def change_image(self, path):
+        self.image_path = self.base_path + path
+        print( self.image_path)
+
     def logout(self):
         resp = Storage.logoff()
         if resp:

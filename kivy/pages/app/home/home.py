30b6b96b46951a7fe13a66fb0e090027b1439681
mateuskb <mateuskb@icloud.com>
2019-09-13 18:12:20 -0300

    Image Loading | Errors=1

--- kivy/pages/app/home/home.py
+++ kivy/pages/app/home/home.py
@@ -1,9 +1,11 @@
 import sys, os
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.widget import Widget
+from kivy.uix.image import AsyncImage
 from kivy.uix.screenmanager import Screen
-from kivy.properties import StringProperty
-
+from kivy.properties import StringProperty, ListProperty
+import pandas as pd
 
 BASE_PATH = os.path.abspath(__file__+ '/../../../../')
 
@@ -18,25 +20,40 @@ Builder.load_file(BASE_PATH + '/pages/app/home/home.kv')
 # class FullImage(Image):
 #         pass
 
-
 class HomeWindow(Screen):
 
-    base_path = 'http://image.tmdb.org/t/p/w185/'
-    image_path = base_path + '16XOMpEaLWkrcPqSQqhTmeJuqQl.jpg'
+    mv_data = pd.read_csv(BASE_PATH + '/inc/test_data/20movies.csv')[:1]
+
+    base_path = 'http://image.tmdb.org/t/p/w185'
     
+    images_urls = [
+        StringProperty(''),
+        StringProperty('')
+    ]
+    
+    a = 0
+    for url in images_urls:
+        images_urls[a] = 'http://image.tmdb.org/t/p/w185//A3aYGp8LLxuFdzG2ETnhfbWPk7h.jpg'
+        a += 1
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         # Check for login
         resp = Storage.r_authtoken()
         if not resp:
             self.logout()   
+            
 
     def consts(self):
         return Consts()
     
-    def change_image(self, path):
-        self.image_path = self.base_path + path
-        print( self.image_path)
+    def load_movies(self):
+        print(self.images_urls[0])
+
+        for index, row in self.mv_data.iterrows():
+            self.images_urls[index] = self.base_path + row['con_c_image_path']
+
+        print(self.images_urls[0])
 
     def logout(self):
         resp = Storage.logoff()
